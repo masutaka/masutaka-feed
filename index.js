b1ad3977a6f2e79b73812c94de3c7e389a8d4f19
Takashi Masuda <masutaka.net@gmail.com>
2020-06-21 23:18:45 +0900

    The tweet succeeded

---
+++ index.js
@@ -0,0 +1,22 @@
+const twitter = require("twitter");
+
+const twitter_client = new twitter({
+  consumer_key: process.env.TWITTER_API_KEY,
+  consumer_secret: process.env.TWITTER_API_SECRET_KEY,
+  access_token_key: process.env.TWITTER_ACCESS_TOKEN,
+  access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET,
+});
+
+exports.handler = (event, context, callback) => {
+  twitter_client.post(
+    "statuses/update",
+    {
+      status: "この 3 ヶ月でどれだけの炭酸水を飲んだだろう "
+    },
+    (error, tweet, response) => {
+      if(error) {
+        callback(null, "Twitter bot error.");
+      }
+      callback(null, "Twitter bot end.");
+    });
+};
